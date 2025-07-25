b793d1d382a65a4379dffa003f89da9571f51821
TvickyPace <156787980+TvickyPace@users.noreply.github.com>
2025-07-25 22:56:17 +0530

    Feat: navbar is responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,8 +6,8 @@ function Navbar() {
   const content = getComponentTexts("navbar");
 
   return (
-    <div className="h-[160px] relative">
-      <div className="absolute left-1/2 top-[30px] -translate-x-1/2">
+    <div className="h-[100px] md:h-[120px] lg:h-[160px] relative">
+      <div className="absolute left-1/2 top-[20px] md:top-[30px] -translate-x-1/2">
         <div className="bg-white/[0.03] rounded-[16px] py-[6px] px-[20px] flex gap-x-[18px]">
           {content?.navbarList?.map((item, index) => {
             return (
