7d7b1e10dfa24e6b6aa6c62ac85720269fede6c0
TvickyPace <156787980+TvickyPace@users.noreply.github.com>
2025-07-25 22:41:15 +0530

    feat: navbar is added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { getComponentTexts } from "@/utilities/commonFunctions";
 import React from "react";
+import NavbarLink from "./NavbarLink";
 
 function Navbar() {
   const content = getComponentTexts("navbar");
@@ -7,10 +8,17 @@ function Navbar() {
   return (
     <div className="h-[160px] relative">
       <div className="absolute left-1/2 top-[30px] -translate-x-1/2">
-        <div className="bg-white/[0.03] rounded-[16px] px-[20px] flex gap-x-[18px]">
-          {/* {content.map((item, index) => {
-            return;
-          })} */}
+        <div className="bg-white/[0.03] rounded-[16px] py-[6px] px-[20px] flex gap-x-[18px]">
+          {content?.navbarList?.map((item, index) => {
+            return (
+              <NavbarLink
+                key={index}
+                name={item.name}
+                icon={item.icon}
+                link={item.link}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
