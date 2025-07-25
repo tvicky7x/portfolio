8784bb82bd140fd15362aa05075b2f3f68cd1118
TvickyPace <156787980+TvickyPace@users.noreply.github.com>
2025-07-25 21:37:48 +0530

    Feat: bug is being resloved

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,20 @@
+import { getComponentTexts } from "@/utilities/commonFunctions";
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const content = getComponentTexts("navbar");
+
+  return (
+    <div className="h-[160px] relative">
+      <div className="absolute left-1/2 top-[30px] -translate-x-1/2">
+        <div className="bg-white/[0.03] rounded-[16px] px-[20px] flex gap-x-[18px]">
+          {/* {content.map((item, index) => {
+            return;
+          })} */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
